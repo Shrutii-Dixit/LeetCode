7a663bafd52c7072af1009dee0eeee31075a89ec
Shruti Dixit <shrutii.dixit.23@gmail.com>
2026-04-02 01:55:00 +0530

    Time: 3 ms (85.03%) | Memory: 43.8 MB (35.35%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        
+        // Edge case
+        if(num1.equals("0") || num2.equals("0")) return "0";
+
+        int n = num1.length();
+        int m = num2.length();
+
+        int[] result = new int[n + m];
+
+        // Multiply digits
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = m - 1; j >= 0; j--){
+                
+                int d1 = num1.charAt(i) - '0';
+                int d2 = num2.charAt(j) - '0';
+
+                int product = d1 * d2;
+
+                int pos = i + j + 1;
+
+                int sum = product + result[pos];
+
+                result[pos] = sum % 10;          
+                result[pos - 1] += sum / 10;     
+            }
+        }
+
+        // Convert array to string
+        StringBuilder sb = new StringBuilder();
+
+        for(int num : result){
+            if(!(sb.length() == 0 && num == 0)){  
+                sb.append(num);
+            }
+        }
+
+        return sb.toString();
+    }
+}
