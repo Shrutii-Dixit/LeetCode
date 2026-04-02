93c5717e11020f20978001dce7f49349d9701cc5
Shruti Dixit <shrutii.dixit.23@gmail.com>
2026-04-02 14:04:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (85.21%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        int n = address.length();
+        StringBuilder finall = new StringBuilder();
+        for(int i=0; i<n; i++){
+            char ch = address.charAt(i);
+            if(ch != '.') finall.append(ch);
+            else{
+                finall.append('[');
+                finall.append('.');
+                finall.append(']');
+            }
+        }
+        return finall.toString();
+    }
+}
