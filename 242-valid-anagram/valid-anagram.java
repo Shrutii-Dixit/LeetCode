2181805d54cfcc7b0ac0a0d1037dcef8a534b6a8
Shruti Dixit <shrutii.dixit.23@gmail.com>
2026-06-09 12:54:24 +0530

    Time: 7 ms (33.67%) | Memory: 44.3 MB (81.85%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] count = new int[26];
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            count[c1 - 'a']++;
+            count[c2 - 'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+}
